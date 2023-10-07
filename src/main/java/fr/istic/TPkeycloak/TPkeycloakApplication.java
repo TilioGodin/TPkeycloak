1d534e21d5948065623a25e228d8003ee83978f9
Tilio Godin <tilio.godin@etudiant.univ-rennes1.fr>
2023-10-07 14:29:51 +0200

    Initial commit

---
+++ src/main/java/fr/istic/TPkeycloak/TPkeycloakApplication.java
@@ -0,0 +1,13 @@
+package fr.istic.TPkeycloak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TPkeycloakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TPkeycloakApplication.class, args);
+	}
+
+}
